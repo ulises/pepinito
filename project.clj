104a7b927a7c4c46d5638dec22a69b4842710210
Ulises Cervino Beresi <ulises@cloudant.com>
2013-04-29 21:00:00 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject pepinito "0.0.1-SNAPSHOT"
+  :description "An implementation of a subset of Python's pickle format."
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
