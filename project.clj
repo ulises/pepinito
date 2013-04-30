de3487b4b10c786dd73b66b4b1a30bfe27bd4e8c
Ulises Cervino Beresi <ulises@cloudant.com>
2013-04-30 16:25:12 +0100

    Add logging and warn on reflection.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject pepinito "0.0.1-SNAPSHOT"
   :description "An implementation of a subset of Python's pickle format."
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  :warn-on-reflection true)
