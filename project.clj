66f227d976da48af9887df01c26b42f52cfb9d1f
Ulises Cervino Beresi <ulises@cloudant.com>
2013-05-02 16:53:18 +0100

    Add midje and kibit.

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
   :description "An implementation of a subset of Python's pickle format."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.6"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]
+                   :plugins [[lein-midje "3.0.0"]
+                             [lein-kibit "0.0.8"]]}}
   :warn-on-reflection true)
