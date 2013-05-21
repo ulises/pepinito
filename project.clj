6c44471be55f595f50ba7fc03fe6d0aaa54cc64e
Ulises Cervino Beresi <ulises@cloudant.com>
2013-05-21 11:06:08 +0100

    Remove dependency on logging.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject pepinito "0.0.1-SNAPSHOT"
   :description "An implementation of a subset of Python's pickle format."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.6"]]
+  :url "http://github.com/ulises/pepinito"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]
                    :plugins [[lein-midje "3.0.0"]
                              [lein-kibit "0.0.8"]]}}
