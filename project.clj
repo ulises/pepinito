dd2eda97cdab3aa363361f6e6b4f46c907341395
Ulises Cervino Beresi <ulises@cloudant.com>
2013-05-22 12:16:58 +0100

    Add license, proper reflection warnings and deploying instructions.

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject pepinito "0.0.1-SNAPSHOT"
   :description "An implementation of a subset of Python's pickle format."
   :url "http://github.com/ulises/pepinito"
+  :license {:name "Apache License Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]
                    :plugins [[lein-midje "3.0.0"]
                              [lein-kibit "0.0.8"]]}}
-  :warn-on-reflection true)
+  :global-vars {*warn-on-reflection* true}
+  :deploy-branches ["master"])
